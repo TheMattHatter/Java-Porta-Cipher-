0987d1ddd0fb6ca72e31d174a687f62c381269bc
TheMattHatter <G00338622@gmit.ie>
2017-04-11 16:22:11 +0100

    Wrote FileParser

--- src/ie/gmit/sw/FileParser.java
+++ src/ie/gmit/sw/FileParser.java
@@ -1,5 +1,91 @@
 package ie.gmit.sw;
 
-public class FileParser {
-	//The file parser will go here
+import java.io.*;
+import java.util.*;
+
+public class FileParser implements Parserator {
+	
+	private BufferedReader br = null; //Provides efficient reading of all characters, lines etc. in the File
+	
+    private String s = null; //Used for separation and storing of strings into the ArrayList
+    
+    private List<String> fileContents = new ArrayList<String>(); //ArrayList created to store all contents of parsed File
+    
+    
+	
+	/***************************************
+	 
+	 ======================
+	 Section 1 File Parsing
+	 ======================
+	  
+	 *Default constructors and File Parsing
+	 
+	 
+	****************************************/
+	
+	//Default constructor//
+	public FileParser()
+	{
+		
+	}
+	
+	//Passing in a file//
+	public FileParser(File file)
+	{
+		
+	}
+	
+	//Parserator (interface) method//
+	public void parse(String s1)
+	{
+		this.parse(new File (s1)); //Casting String as file and parsing it
+	}
+	
+	//File parsing//
+	public void parse(File file)
+	{
+		
+		if(file.isFile()) //File validation
+		{
+			
+			try
+			{
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			}
+			
+			catch(FileNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+			
+			
+	        //Try to separate words and store in the ArrayList
+	        try
+	        {
+		        while((s = br.readLine()) != null)
+		  	  {		  
+		  		  String [] str = s.split("\\s+"); //Groups all white spaces as a delimiter
+		  		 
+		  		  fileContents.addAll(Arrays.asList(str));  //Adding all strings to the ArrayList
+		  	  }
+		        
+	        }
+	        
+	        catch(IOException e)
+	        {
+	  		 e.printStackTrace();
+	        }
+	        
+			System.out.println("\nFile successfully read!");  //Printed if file parse is successfully
+
+	        
+		}//End if
+		
+		else
+		{
+			System.out.println("\nThat file does not exist please try again!");  //Printed if file does not exist
+		}
+		
+	}
 }
--- src/ie/gmit/sw/Parserator.java
+++ src/ie/gmit/sw/Parserator.java
@@ -1,5 +1,7 @@
 package ie.gmit.sw;
 
-public class Parserator {
-	//The parsing interface will go here
+public interface Parserator {
+	
+	public void parse(String s1); //Parses a file/URL
+	
 }
